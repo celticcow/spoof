e522fa0e0ce2c48ec335ef77fffce5e379b68295
Gregory Dunlap <gregory.dunlap@fedex.com>
2021-03-09 17:00:46 -0600

    gets list of spoof groups into list

--- spoof_find_add.py
+++ spoof_find_add.py
@@ -21,16 +21,17 @@ def login_api(key, mds, domain):
 
 def get_cluster_names(mds, sid):
     #show simple-clusters limit 5 --format json | jq '.objects[] | .name'
-    debug = 1
+    debug = 0
     cluster_names = list()
 
     show_simple_cluster_json = {
-        "limit" : 5
+        "limit" : 300
     }
 
     show_simple_cluster_result = apifunctions.api_call(mds, "show-simple-clusters", show_simple_cluster_json, sid)
 
-    print(json.dumps(show_simple_cluster_result))
+    if(debug == 1):
+        print(json.dumps(show_simple_cluster_result))
 
     for x in range(show_simple_cluster_result['total']):
         cluster_names.append(show_simple_cluster_result['objects'][x]['name'])
@@ -46,45 +47,55 @@ def get_spoof_from_name(mds, sid, cluster_name):
         "name" : cluster_name
     }
 
+    spoof_groups = list()
+
     show_simple_cluster_result = apifunctions.api_call(mds, "show-simple-cluster", show_simple_cluster_json, sid)
 
     if(debug == 1):
         print(json.dumps(show_simple_cluster_result))
 
     for x in range(show_simple_cluster_result['interfaces']['total']):
-        print(show_simple_cluster_result['interfaces']['objects'][x])
-        
-        print("\n")
-        print(show_simple_cluster_result['interfaces']['objects'][x]['name'])
-        print(show_simple_cluster_result['interfaces']['objects'][x]['interface-type'])
+        if(debug == 1):
+            print(show_simple_cluster_result['interfaces']['objects'][x])
+            
+            print("\n")
+            print(show_simple_cluster_result['interfaces']['objects'][x]['name'])
+            print(show_simple_cluster_result['interfaces']['objects'][x]['interface-type'])
 
         if(show_simple_cluster_result['interfaces']['objects'][x]['interface-type'] == "cluster" or show_simple_cluster_result['interfaces']['objects'][x]['interface-type'] == "cluster + sync"):
-            print("HIT")
-            print("\U0001F603")
+            #print("HIT")
+            #print("\U0001F603")
 
+            print("Spoof_Group for : " + cluster_name)
+            print(show_simple_cluster_result['interfaces']['objects'][x]['name'])
             try:
-                print(show_simple_cluster_result['interfaces']['objects'][x]['interface-type'])
+                print(show_simple_cluster_result['interfaces']['objects'][x]['interface-type'] + " interface")
                 print(show_simple_cluster_result['interfaces']['objects'][x]['topology-settings']['specific-network'])
+                spoof_groups.append(show_simple_cluster_result['interfaces']['objects'][x]['topology-settings']['specific-network'])
             except:
-                print("settings")
+                print("NO-Specific-Spoof-Group")
+        print("-" * 40)
+    return(spoof_groups)
+#end of get_spoof_from_name
 
 def main():
-    print("Start")
-    debug = 1
-    mds = "146.18.96.16"
-    cma = "146.18.96.28"
+    print("Spoof Finder")
+    debug = 0
+    mds = "146.18.96.16"#"192.168.159.150"  # "146.18.96.16"
+    cma = "146.18.96.28"#"192.168.159.155"  # "146.18.96.28"
 
-    clusters = list()
+    clusters    = list()
+    all_spoofs  = list()
 
     key = {}
-    with open('apirw-key.json', 'r') as f:
+    with open('main-api-rw-key.json', 'r') as f:
         key = json.load(f)
 
         if(debug == 1):
             print(key['api-key'])
     
     #sid = login_api(key['api-key'], mds, cma)
-    sid = apifunctions.login("gdunlap", "1qazxsw2", mds, cma)
+    sid = apifunctions.login("gp", "12", mds, cma)
 
     if(debug == 1):
         print("login sid : " + sid)
@@ -96,7 +107,13 @@ def main():
         print(clusters)
     
     for cluster in clusters:
-        get_spoof_from_name(mds, sid, cluster)
+        tmp_spoof = get_spoof_from_name(mds, sid, cluster)
+        for spoof in tmp_spoof:
+            all_spoofs.append(spoof)
+
+    print("+"*30)
+    print(all_spoofs)
+    print("+"*30)
 
     time.sleep(20)
     logout_result = apifunctions.api_call(mds, "logout", {}, sid)
