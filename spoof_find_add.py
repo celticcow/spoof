1b90db1aaab2ef634b56309004e72feb4dbb5ae4
Gregory Dunlap <gregory.dunlap@fedex.com>
2021-03-09 00:10:16 -0600

    traverse working but rough

--- spoof_find_add.py
+++ spoof_find_add.py
@@ -19,10 +19,76 @@ def login_api(key, mds, domain):
     return response["sid"]
 #end of api
 
+def get_cluster_names(mds, sid):
+    #show simple-clusters limit 5 --format json | jq '.objects[] | .name'
+    debug = 1
+    cluster_names = list()
+
+    show_simple_cluster_json = {
+        "limit" : 5
+    }
+
+    show_simple_cluster_result = apifunctions.api_call(mds, "show-simple-clusters", show_simple_cluster_json, sid)
+
+    print(json.dumps(show_simple_cluster_result))
+
+    for x in range(show_simple_cluster_result['total']):
+        cluster_names.append(show_simple_cluster_result['objects'][x]['name'])
+
+    return(cluster_names)
+#end of get_cluster_names
+
+def get_spoof_from_name(mds, sid, cluster_name):
+    #show simple-cluster name ALBA122 --format json | jq '."interfaces" | .objects[] | ."topology-settings"'
+    debug = 0
+    
+    show_simple_cluster_json = {
+        "name" : cluster_name
+    }
+
+    show_simple_cluster_result = apifunctions.api_call(mds, "show-simple-cluster", show_simple_cluster_json, sid)
+
+    if(debug == 1):
+        print(json.dumps(show_simple_cluster_result))
+
+    for x in range(show_simple_cluster_result['interfaces']['total']):
+        print(show_simple_cluster_result['interfaces']['objects'][x])
+        print("\n")
+
 def main():
     print("Start")
     debug = 1
+    mds = "146.18.96.16"
+    cma = "146.18.96.28"
 
+    clusters = list()
+
+    key = {}
+    with open('apirw-key.json', 'r') as f:
+        key = json.load(f)
+
+        if(debug == 1):
+            print(key['api-key'])
+    
+    #sid = login_api(key['api-key'], mds, cma)
+    sid = apifunctions.login("a", "1", mds, cma)
+
+    if(debug == 1):
+        print("login sid : " + sid)
+    
+    clusters = get_cluster_names(mds, sid)
+
+    if(debug == 1):
+        print("*" *20)
+        print(clusters)
+    
+    for cluster in clusters:
+        get_spoof_from_name(mds, sid, cluster)
+
+    time.sleep(20)
+    logout_result = apifunctions.api_call(mds, "logout", {}, sid)
+    print("logout results : " + json.dumps(logout_result))
+#end of main
 
 if __name__ == "__main__":
     main()
